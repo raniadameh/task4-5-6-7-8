7a87e19d41aee16cbf9a8b686efc08d3e297ef37
raniadameh <raniadameh2014@gmail.com>
2022-07-13 14:15:09 +0300

    task 6

--- src/app/create-update-post/create-update-post.component.ts
+++ src/app/create-update-post/create-update-post.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { identityRevealedValidator } from '../create-update-user-reactive/customValidator';
 import { UserPostsService } from '../services/user-posts.service';
-import { Location } from '@angular/common';
+import { formatCurrency, Location } from '@angular/common';
 import { Post, PostPreview } from '../models/post.model';
 import { List } from '../models/user.model';
 
@@ -23,7 +23,7 @@ export class CreateUpdatePostComponent implements OnInit {
   postId!: string;
   viewPost!: Post;
 
-  constructor(private location: Location, private postsService: UserPostsService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private formBuilder: FormBuilder, private location: Location, private postsService: UserPostsService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe(params => {
@@ -33,9 +33,10 @@ export class CreateUpdatePostComponent implements OnInit {
     if (this.postId == '') {
       this.postForm = new FormGroup({
         text: new FormControl('', [Validators.required, Validators.minLength(6)]),
-        likes: new FormControl(0),
-        tages: new FormControl(''),
-        image: new FormControl(''),
+        likes: new FormControl(0, [Validators.required]),
+        //tages: new FormControl('', [Validators.required]),
+        tages: new FormArray([], [Validators.required]),
+        image: new FormControl('', [Validators.required]),
         owner: new FormControl(this.ownerId, [Validators.required])
       });
       this.postForm.controls['owner'].disable();
@@ -45,11 +46,13 @@ export class CreateUpdatePostComponent implements OnInit {
         this.viewPost = response;
         this.postForm = new FormGroup({
           text: new FormControl(this.viewPost.text, [Validators.required, Validators.minLength(6)]),
-          likes: new FormControl(this.viewPost.likes),
-          tages: new FormControl(this.viewPost.tags),
-          image: new FormControl(this.viewPost.image),
+          likes: new FormControl(this.viewPost.likes, [Validators.required]),
+          //tages: new FormControl(this.viewPost.tags, [Validators.required]),
+          tages: new FormArray([], [Validators.required]),
+          image: new FormControl(this.viewPost.image, [Validators.required]),
           owner: new FormControl(this.ownerId, [Validators.required])
         });
+        this.viewPost.tags.forEach(tag => { this.addTages(tag) })
         this.postForm.controls['owner'].disable();
       }
       );
@@ -58,45 +61,48 @@ export class CreateUpdatePostComponent implements OnInit {
 
   }
   onSubmit() {
-    if (this.postId == '') {
-      this.postsService.createPost({
-        text: this.text?.value,
-        image: this.image?.value,
-        likes: 0,
-        tags: this.tages?.value,
-        owner: this.ownerId
-      })
-        .subscribe({
-          next: response => {
-            this.sub = true;
-            setTimeout(() => {
-              this.back();
-            }, 3000);
-          },
-          error: (err) => {
-            this.err = 'Error Occurred', this.done = true, this.sub = false
-          }
-        });
-    }
-    else if (this.postId != '') {
-      const newAccount = {
-        text: this.text?.value,
-        image: this.image?.value,
-        tages: this.tages?.value,
-        likes: this.likes?.value
-      };
-      this.postsService.updatePost(this.postId, newAccount)
-        .subscribe({
-          next: response => {
-            this.sub1 = true;
-            setTimeout(() => {
-              this.back();
-            }, 3000);
-          },
-          error: (err) => {
-            this.err = 'Error Occurred when updated';
-          }
-        });
+    this.postForm.markAllAsTouched();
+    if (this.postForm.valid) {
+      if (this.postId == '') {
+        this.postsService.createPost({
+          text: this.text?.value,
+          image: this.image?.value,
+          likes: 0,
+          tags: this.tages?.value,
+          owner: this.ownerId
+        })
+          .subscribe({
+            next: response => {
+              this.sub = true;
+              setTimeout(() => {
+                this.back();
+              }, 3000);
+            },
+            error: (err) => {
+              this.err = 'Error Occurred', this.done = true, this.sub = false
+            }
+          });
+      }
+      else if (this.postId != '') {
+        const newAccount = {
+          text: this.text?.value,
+          image: this.image?.value,
+          tags: this.tages?.value,
+          likes: this.likes?.value
+        };
+        this.postsService.updatePost(this.postId, newAccount)
+          .subscribe({
+            next: response => {
+              this.sub1 = true;
+              setTimeout(() => {
+                this.back();
+              }, 3000);
+            },
+            error: (err) => {
+              this.err = 'Error Occurred when updated';
+            }
+          });
+      }
     }
   }
   back() {
@@ -110,7 +116,14 @@ export class CreateUpdatePostComponent implements OnInit {
     return this.postForm.get('likes');
   }
   get tages() {
-    return this.postForm.get('tages');
+    return this.postForm.get('tages') as FormArray;
+  }
+  addTages(tag1: any) {
+    this.tages.push(this.formBuilder.control(tag1));
+  }
+  removeTages(i: number) {
+    this.tages.removeAt(i);
+
   }
   get image() {
     return this.postForm.get('image');
